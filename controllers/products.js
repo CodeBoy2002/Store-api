f7b8598ffbd9cf0e4230508f93527b208bc4afae
Sumit <marvelsumit2002@gmail.com>
2023-11-15 22:15:30 +0530

    query not

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,21 @@ const express = require('express')
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res)=> {
-    const products = await Product.find({ name: 'vase table' })
+    const products = await Product.find({})
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res)=> {
-    res.send(req.query)
+    const { featured } = req.query
+    const queryObject = {}
+
+    if(featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    console.log(queryObject);
+    const products = await Product.find(queryObject)
+    res.status(200).json({ products, nbHits: products.length })
 }
  
 module.exports = {
